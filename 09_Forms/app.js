3733f295d9680edec496f62f1bbe75803b32d6bf
Luis Molina <luisame@outlook.com>
2017-05-16 12:38:11 -0500

    ahora ya voy con Lifting State Up

--- 09_Forms/app.js
+++ 09_Forms/app.js
@@ -246,6 +246,309 @@ ReactDOM.render(<EssayForm />,document.getElementById('cont4'))
 
 
 
+//  -------------------------------- El tag select -------------------------------------
+
+// En Html, <select> Crea una lista desplegable. Por ejemplo, este código HTML crea una lista desplegable de sabores:
+
+
+// <select>
+//   <option value="grapefruit">Grapefruit</option>
+//   <option value="lime">Lime</option>
+//   <option selected value="coconut">Coconut</option>
+//   <option value="mango">Mango</option>
+// </select>
+
+
+// Tenga en cuenta que la opción Coconut se selecciona inicialmente, debido al atributo selected . 
+// React, en lugar de utilizar este atributo selected, utiliza el atributo value en la 
+// etiqueta de raíz del select. Esto es más conveniente en un componente controlado porque solo es 
+// necesario actualizarlo en un solo lugar. Por ejemplo:
+
+
+// Mi ejemplo realizado por mi mismo
+
+class SelectSabor extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+
+		this.state = ({sabor:'vainilla'});
+
+		this.envioSabor = this.envioSabor.bind(this);
+		this.cambioSabor = this.cambioSabor.bind(this);
+
+	}
+
+	envioSabor(event)
+	{
+		event.preventDefault();
+		alert("Tu sabor preferido es " + this.state.sabor);
+		
+	}
+
+	cambioSabor(event)
+	{
+		this.setState({sabor:event.target.value});
+	}
+
+	render()
+	{
+		const tmpSelectSabor = (
+								<form onSubmit={this.envioSabor} >
+									<label>
+										Seleciona tu sabor favorito :
+									</label>
+									<select  onChange={this.cambioSabor} value={this.state.sabor} >
+										<option value="limon" >Limon</option>
+										<option value="fresa">Fresa</option>
+										<option value="vainilla">Vainilla</option>
+										<option value="chocolate">Chocolate</option>
+									</select>
+									<button type="submit" >Enviar</button> 
+								</form>
+						 );
+
+		return tmpSelectSabor;
+	}
+
+}
+
+
+ReactDOM.render(<SelectSabor />,document.getElementById('cont5'));
+
+
+// ejemplo de la documentacion de React de Facebook
+
+
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite La Croix flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <FlavorForm />,
+  document.getElementById('cont6')
+);
+
+
+// En general, esto hace que <input type="text">, <textarea> , y <select> Todos funcionen de manera muy 
+// similar - todos ellos aceptan un atributo de value que puede utilizar para implementar un componente controlado.
+
+
+
+// ---------------------------- Manejo de múltiples entradas --------------------------------------
+
+
+// Cuando necesite manejar varios elementos de entrada controlados, puedes agregar un atributo "name" a
+//  cada elemento y dejar que la función del manejador elija qué hacer en función del valor de event.target.name.
+
+// Por ejemplo: 
+
+
+// ejemplo creado por mi mismo
+
+class EnviarReservacion extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+
+		this.state = ({checkGoHotel: true, numberVisits: 1});
+
+		this.cambioSomeForm = this.cambioSomeForm.bind(this); 
+		this.enviado = this.enviado.bind(this); 
+	}
+
+	cambioSomeForm(event)
+	{
+
+		const target = event.target;
+		var valor = "";
+		if(target.type === 'checkbox')
+		{
+			valor = target.checked;
+		}
+		else
+		{
+			valor = target.value;
+		}
+		const nombre = target.name;
+
+		this.setState({[nombre]:valor});
+
+
+	}
+
+	enviado(event)
+	{
+		event.preventDefault();
+		alert("usted eligio " + this.state.checkGoHotel + " con numero de visitantes " + this.state.numberVisits);
+	}
+
+	render()
+	{	
+		const tmpEnvioReservacion = (
+										<form onSubmit={this.enviado}>
+											<label >ir a hotel:</label>
+											<input type="checkbox" name="checkGoHotel" checked={this.state.checkGoHotel} onChange={this.cambioSomeForm} />
+											<br />
+											<label >Numero de visitantes</label>
+											<input type="number" name="numberVisits" value={this.state.numberVisits} onChange={this.cambioSomeForm} />		
+											<button type="submit">Enviar</button>
+										</form>
+									 );
+		
+		return tmpEnvioReservacion;
+
+	}
+}
+
+
+ReactDOM.render(<EnviarReservacion />,document.getElementById('cont7'));
+
+
+// Ejemplo de la documentacion de React de Facebook
+
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Reservation />,
+  document.getElementById('cont8')
+);
+
+
+
+
+// Observe cómo usamos la sintaxis de ES6 "computed property name"  "de nombre de propiedad calculada"  
+//para actualizar la key de estado  correspondiente al nombre de entrada dado:
+
+// this.setState({
+//   [name]: value
+// });
+
+
+// Es equivalente a este código ES5:
+
+// var partialState = {};
+// partialState[name] = value;
+// this.setState(partialState);
+
+
+// Además, dado que setState () fusiona automáticamente un estado parcial con el estado actual, 
+// sólo necesitábamos llamarlo con las partes modificadas.
+
+
+// -------------------- Alternativas a los componentes controlados --------------------------------
+
+
+// A veces puede ser tedioso utilizar componentes controlados, ya que necesita escribir un controlador 
+// de eventos para cada elemento en el que sus datos pueden cambiar y canalizar todo el estado de entrada a través 
+// de un componente React. Esto puede llegar a ser particularmente molesto cuando se está convirtiendo una
+//  base de código preexistente a React, o la integración de una aplicación de React con una biblioteca
+//   de no-React. En estas situaciones, es posible que desee comprobar componentes no controlados, 
+// https://facebook.github.io/react/docs/uncontrolled-components.html
+//   una técnica alternativa para implementar formularios de entrada.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
