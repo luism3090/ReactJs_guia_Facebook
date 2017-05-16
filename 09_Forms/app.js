2657daf01df95316f1efacb81de20b8c96795e40
Luis Molina <luisame@outlook.com>
2017-05-16 00:35:05 -0500

    Sigo con forms voy en the select tag

--- 09_Forms/app.js
+++ 09_Forms/app.js
@@ -82,6 +82,10 @@ class EnviarForm extends React.Component
 
 }
 
+ReactDOM.render(<EnviarForm />,document.getElementById('cont1'));
+
+
+
 // ejemplo de la documentacion de React de facebook 
 
 class NameForm extends React.Component {
@@ -117,30 +121,128 @@ class NameForm extends React.Component {
 
 
 
-ReactDOM.render(<EnviarForm />,document.getElementById('cont1'));
+ReactDOM.render(<EnviarForm />,document.getElementById('cont2'));
 
 
 // Dado que el atributo value se establece en nuestro elemento de formulario, el valor que se mostrará siempre 
 // será this.state.value, haciendo que el estado React sea el estado verdadero o el estado correcto . 
-// Dado que handleChange se ejecuta en cada pulsación de tecla para actualizar el estado React, 
+// Dado que handleChange se ejecuta en cada pulsación de tecla para actualizar el estado de React, 
 // el valor mostrado se actualizará según el tipo de usuario.
 
 
+// Con un componente controlado, cada mutación "Cambio" de estado tendrá una función de controlador asociada. 
+// Esto hace que sea sencillo modificar o validar la entrada del usuario. Por ejemplo, si quisiéramos
+//  hacer cumplir que los nombres se escriban con todas las letras mayúsculas, podríamos escribir 
+//  handleChange como:
+
+
+// handleChange(event) {
+//   this.setState({value: event.target.value.toUpperCase()});
+// }
+
+
+// --------------------------- La etiqueta textarea ------------------------------
+
+
+// En HTML, un elemento <textarea> define su texto por sus hijos 
+
+
+// <textarea>
+//   Hello there, this is some text in a text area
+// </textarea>
+
+// En React, un elemento <textarea> usa el atributo value en su lugar. De esta forma, un formulario usando un 
+// elemento <textarea> Se puede escribir de forma muy similar a un formulario que utiliza una entrada
+//  de una sola línea:
 
 
 
+// Mi ejemplo creado por mi mismo
+
+class EnviarResumen extends React.Component
+{
+	constructor()
+	{
+		super();
+
+		this.state = ({resumen:'Escriba el resumen de su elemento DOM favorito'});
+
+		this.cambiarResumen = this.cambiarResumen.bind(this);
+		this.envioResumen = this.envioResumen.bind(this);
+	}
+
+	cambiarResumen(event)
+	{
+		this.setState({resumen: event.target.value})
+	}
+
+	envioResumen(event)
+	{
+		alert("tu resumen de tu elemento DOM favorito es: "+this.state.resumen);
+		event.preventDefault();
+	}
+
+	render()
+	{
+		const tmpFormEnvioResumen = (
+										<form onSubmit={this.envioResumen}>
+											<label>Resumen:</label>
+										    <textarea cols="30" rows="5" value={this.state.resumen} onChange={this.cambiarResumen} ></textarea>
+										   	<button type="submit">Enviar</button>
+										</form>
+							  		)
+
+		return tmpFormEnvioResumen;
 
 
+	}
+}
+
+ReactDOM.render(<EnviarResumen />,document.getElementById('cont3'));
 
 
+// ejemplo de la documentacion de React js de facebook
 
 
+class EssayForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 
+ReactDOM.render(<EssayForm />,document.getElementById('cont4'))
 
 
-
-
+// Observe que this.state.value se inicializa en el constructor, de modo que el área de texto
+//  comienza con ese texto en él.
 
 
 
