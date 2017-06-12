ffff797d686a463d785339b9a01bd86b196f86a2
Luis Molina <luisame@outlook.com>
2017-06-12 01:09:37 -0500

    siguiendo con guia avanzada react js

---
+++ ADVANCED GUIDES/01_JSX_In_Depth/mi_componente/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export class FeedbackMessage extends React.Component {
+    render() {
+        return (
+            <div>
+                Bienvenido a FeedbackMessage!
+            </div>
+        )
+    }
+
+}
+
+
+
+//ReactDOM.render(<FeedbackMessage />,document.getElementById('cont1'));
